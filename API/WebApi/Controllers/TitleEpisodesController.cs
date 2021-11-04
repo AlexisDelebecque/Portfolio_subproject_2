a47e1dc44a2f64e32994a3c7be191a44194949a2
alexis_delebecque <alexis.delebecque@epitech.eu>
2021-11-04 18:20:36 +0100

    fix: remove dataserviceLib variable

--- API/WebApi/Controllers/TitleEpisodesController.cs
+++ API/WebApi/Controllers/TitleEpisodesController.cs
@@ -1,5 +1,3 @@
-using System;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.Services;
 
--- API/WebApi/Startup.cs
+++ API/WebApi/Startup.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using DataServiceLib;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -18,8 +12,6 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-
-            services.AddSingleton<IDataService, DataService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
